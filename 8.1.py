c85b8af5a994d6aff1c71f1712381ae96400dbfd
Ren <netitokoo@gmail.com>
2024-10-04 10:44:37 +0300

    homework

---
+++ 8.1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a + b
+        else:
+            raise TypeError
+    except TypeError:
+        return str(a) + str(b)
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
